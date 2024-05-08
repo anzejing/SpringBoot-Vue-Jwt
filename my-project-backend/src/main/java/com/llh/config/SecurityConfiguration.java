f20f374da33d52ff0bb5d7257f1c1dfd27624ae4
anzejing <870872819@qq.com>
2024-05-08 23:52:21 +0800

    Jwt请求头校验

--- my-project-backend/src/main/java/com/llh/config/SecurityConfiguration.java
+++ my-project-backend/src/main/java/com/llh/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.llh.config;
 
 import com.llh.entity.RestBean;
 import com.llh.entity.vo.response.AuthorizeVo;
+import com.llh.filter.JwtAuthorizeFilter;
 import com.llh.utils.JwtUtils;
 import jakarta.annotation.Resource;
 import jakarta.servlet.ServletException;
@@ -9,6 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +18,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
 
@@ -24,6 +27,8 @@ public class SecurityConfiguration {
 
     @Resource
     JwtUtils utils;
+    @Resource
+    JwtAuthorizeFilter jwtAuthorizeFilter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
@@ -40,11 +45,28 @@ public class SecurityConfiguration {
                         .logoutUrl("/api/auth/logout")
                         .logoutSuccessHandler(this::onLogoutSuccess)
                 )
+                .exceptionHandling(conf->conf
+                        .authenticationEntryPoint(this::onUnauthorized)
+                        .accessDeniedHandler(this::onAccessDeny)
+                )
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(conf->conf
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(jwtAuthorizeFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
+    public void onAccessDeny(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(RestBean.forbidden(accessDeniedException.getMessage()).asJsonString());
+    }
+
+    public void onUnauthorized(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(RestBean.unauthorized(authException.getMessage()).asJsonString());
+
+    }
+
 
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         response.setContentType("application/json");
@@ -62,7 +84,7 @@ public class SecurityConfiguration {
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         response.setContentType("application/json");
         response.setCharacterEncoding("utf-8");
-        response.getWriter().write(RestBean.failure(401, exception.getMessage()).asJsonString());
+        response.getWriter().write(RestBean.unauthorized(exception.getMessage()).asJsonString());
     }
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
--- my-project-backend/src/main/java/com/llh/utils/JwtUtils.java
+++ my-project-backend/src/main/java/com/llh/utils/JwtUtils.java
@@ -1,14 +1,20 @@
 package com.llh.utils;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Map;
 
 @Component
 public class JwtUtils {
@@ -17,6 +23,20 @@ public class JwtUtils {
     String key;
     @Value("${spring.security.jwt.expire}")
     int expire;
+
+    public DecodedJWT resolveJwt(String headerToken){
+        String token = this.convertToken(headerToken);
+        if(token == null)return null;
+        Algorithm algorithm = Algorithm.HMAC256(key);
+        JWTVerifier jwtVerifier = JWT.require(algorithm).build();
+        try {
+            DecodedJWT verify = jwtVerifier.verify(token);
+            Date expiresAt = verify.getExpiresAt();
+            return new Date().after(expiresAt)?null:verify;
+        } catch (JWTVerificationException e) {
+            return null;
+        }
+    }
 
     public String createJwt(UserDetails details,int id,String username){
         Algorithm algorithm = Algorithm.HMAC256(key);
@@ -35,5 +55,26 @@ public class JwtUtils {
         Calendar calendar  = Calendar.getInstance();
         calendar.add(Calendar.HOUR,expire*24);
         return calendar.getTime();
+    }
+
+    public  UserDetails toUser(DecodedJWT jwt){
+        Map<String, Claim> claims = jwt.getClaims();
+        return User
+                .withUsername(claims.get("name").asString())
+                .password("********")
+                .authorities(claims.get("authorities").asArray(String.class))
+                .build();
+    }
+    public Integer toId(DecodedJWT jwt){
+        Map<String, Claim> claims = jwt.getClaims();
+        return claims.get("id").asInt();
+    }
+
+    private String convertToken(String headerToken){
+        if(headerToken == null || !headerToken.startsWith("Bearer ")){
+            return null;
+        }
+        return headerToken.substring(7);
+
     }
 }
