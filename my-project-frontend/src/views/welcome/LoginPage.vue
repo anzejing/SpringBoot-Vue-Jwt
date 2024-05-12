591f6ca48d1081b1a9c8ed84c68df16e750a645c
anzejing <870872819@qq.com>
2024-05-12 20:12:37 +0800

    登录页面

--- my-project-frontend/src/views/welcome/LoginPage.vue
+++ my-project-frontend/src/views/welcome/LoginPage.vue
@@ -1,10 +1,60 @@
 <script setup>
+import {reactive} from "vue";
+import {User,Lock} from '@element-plus/icons-vue'
+
+const form = reactive({
+  username: '',
+  password: '',
+  remember: false
+})
+  
+
 
 </script>
 
 <template>
-  <div>
-    登录页面
+  <div style="text-align: center;margin: 0 20px">
+    <div style="margin-top: 150px">
+      <div style="font-size: 25px;font-weight: bold">登录</div>
+      <div style="font-size: 14px;color: gray">输入用户名密码登录</div>
+    </div>
+    <div style="margin-top: 50px">
+      <el-form v-model="form">
+        <el-form-item>
+          <el-input v-model="form.username" maxlength="10" type="text" placeholder="用户名或邮箱">
+            <template #prefix>
+              <el-icon><User /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="form.password" maxlength="20" placeholder="密码">
+            <template #prefix>
+              <el-icon><Lock /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-row>
+          <el-col :span="12" style="text-align: left">
+            <el-form-item>
+              <el-checkbox v-model="form.remember" label="记住我"/>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12" style="text-align: right">
+            <el-link>忘记密码?</el-link>
+          </el-col>
+        </el-row>
+      </el-form>
+    </div>
+    <div style="margin-top: 40px">
+      <el-button style="width: 270px" type="success" plain>立即登录</el-button>
+    </div>
+    <el-divider>
+      <span style="font-size: 13px;color: gray">没有账号</span>
+    </el-divider>
+    <div>
+     <el-button style="width: 270px" type="warning" plain>立即注册</el-button>
+    </div>
   </div>
 
 </template>
