73015ebc89bfd81b4c882579609498aab519adfd
anzejing <870872819@qq.com>
2024-05-12 22:41:56 +0800

    退出登录和路由守卫

--- my-project-frontend/router/index.js
+++ my-project-frontend/router/index.js
@@ -1,4 +1,5 @@
 import {createRouter, createWebHistory} from "vue-router"
+import {unauthorized} from "@/net/index.js";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,9 +16,26 @@ const router = createRouter({
                 }
 
             ]
+        },
+        {
+            path: '/index',
+            name: 'index',
+            component:() => import('@/views/indexView.vue')
+
         }
     ]
 
+})
+
+router.beforeEach((to,from,next) =>{
+    const isUnauthorized = unauthorized()
+    if(to.name.startsWith('welcome-') && !isUnauthorized){
+        next('/index')
+    }else if (to.path.startsWith('/index') && isUnauthorized){
+        next('/')
+    }else {
+        next()
+    }
 })
 
 export default router
--- my-project-frontend/src/net/index.js
+++ my-project-frontend/src/net/index.js
@@ -39,6 +39,14 @@ function deleteAccessToken(){
     sessionStorage.removeItem(authItemName)
 }
 
+function accessHeader(){
+    const token = takeAccessToken()
+
+    return token ?{
+        'Authorization': `Bearer ${token}`
+    } : {}
+}
+
 function internalPost(url,data,header,success,failure,error = defaultError) {
     axios.post(url,data,{headers: header}).then(({data})=>{
         if(data.code === 200){
@@ -62,6 +70,14 @@ function internalGet(url,header,success,failure,error = defaultError) {
 
 }
 
+function get(url,success,failure = defaultFailure){
+    internalGet(url,accessHeader(),success,failure)
+}
+
+function post(url,data,success,failure = defaultFailure){
+    internalPost(url,data,accessHeader(),success,failure)
+}
+
 function login(username,password,remember,success,failure=defaultFailure){
     internalPost('api/auth/login',{
         username: username,
@@ -76,4 +92,16 @@ function login(username,password,remember,success,failure=defaultFailure){
 
 }
 
-export {login}
+function logout(success,failure = defaultFailure){
+    get('api/auth/logout',() =>{
+        deleteAccessToken()
+        ElMessage.success("退出登录成功,欢迎您再次使用.")
+        success()
+    },failure)
+}
+
+function unauthorized(){
+    return !takeAccessToken()
+}
+
+export {login,logout,get,post,unauthorized}
